5292a7b431ce79cdee0dcd2cfa1408c76a2dfa7e
Chris Sewell <chrisj_sewell@hotmail.com>
2021-12-16 17:09:16 +0100

    update

--- conf.py
+++ conf.py
@@ -4,3 +4,4 @@ extensions = ["myst_parser", "sphinx_external_toc"]
 exclude_patterns = [".github", ".tox", ".vscode", "_build"]
 external_toc_exclude_missing = True
 html_theme = "furo"
+suppress_warnings = ["myst.header"]
